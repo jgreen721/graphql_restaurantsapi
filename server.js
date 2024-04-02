8918529b381eb642bf5f4c6c6ed22bc7e504b868
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-04-02 13:42:37 -0700

    save

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4455;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.get("/", (req, res) => {
   console.log("/restaurants pinged");
   res.json({ status: 200, msg: "restaurants api backend" });
